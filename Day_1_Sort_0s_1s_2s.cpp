a94e8eecf832532fe68a8358d5d8112a51bd17c2
Dhivijit Koppuravuri <68179868+dhivijit@users.noreply.github.com>
2024-09-09 14:31:21 +0530

    Update Day_1_Sort_0s_1s_2s.cpp

--- Day_1_Sort_0s_1s_2s.cpp
+++ Day_1_Sort_0s_1s_2s.cpp
@@ -1,3 +1,10 @@
+// Sort an Array of 0s, 1s, and 2s
+
+// - You are given an array arr consisting only of 0s, 1s, and 2s. 
+// - The task is to sort the array in increasing order in linear time (i.e., O(n)) without using any extra space. 
+// - This means you need to rearrange the array in-place.
+
+
 #include <iostream>
 #include<vector>
 
